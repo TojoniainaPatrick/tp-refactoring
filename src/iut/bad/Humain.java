146ca8ea9825a6373920a6c2a1c21bc7a1495d09
Tojoniaina Patrick <tojoniainapatrickr@gmail.com>
2024-04-02 21:10:44 +0300

    details() methode implementation

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -5,5 +5,9 @@ public class Humain {
 	protected String nom;
 	protected String prenom;
 	protected int age;
+	
+	public void details() {
+		System.out.println(nom + " " + prenom + " " + age);
+	}
 
 }
